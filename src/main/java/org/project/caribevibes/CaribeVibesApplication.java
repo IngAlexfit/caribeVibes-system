75cc9649db1c698eaf2fddb20dee7aa7031019f7
analista.sistemas2 <alexpue@hotmail.com>
2025-06-11 11:23:13 -0500

    commit inicial

---
+++ src/main/java/org/project/caribevibes/CaribeVibesApplication.java
@@ -0,0 +1,13 @@
+package org.project.caribevibes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaribeVibesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaribeVibesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/caribevibes/CaribeVibesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.caribevibes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaribeVibesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
